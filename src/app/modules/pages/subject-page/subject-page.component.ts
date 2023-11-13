291d602f4b8448accd1f3c2d71fef719f5a36ffd
Favour Chukwuemeka <kaosemeka@yahoo.com>
2023-11-13 02:34:26 +0100

    bind get and delete subjects services to components

--- src/app/modules/pages/subject-page/subject-page.component.ts
+++ src/app/modules/pages/subject-page/subject-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { SubjectService } from 'src/app/modules';
+import { ISubject } from 'src/app/core';
 
 @Component({
   selector: 'app-subject-page',
   templateUrl: './subject-page.component.html',
   styleUrls: ['./subject-page.component.scss']
 })
-export class SubjectPageComponent {
+export class SubjectPageComponent implements OnInit {
 
+  public subjects: ISubject[] = [];
+  public subjectService: SubjectService = inject(SubjectService);
+
+  ngOnInit(): void {
+    this.getSubjects();
+  }
+  
+  getSubjects() {
+    this.subjects = this.subjectService.getSubjects();
+  }
+  
+  deleteSubject(subject: ISubject) {
+    this.subjectService.deleteSubject(subject.id);
+    this.getSubjects()
+  }
 }
--- src/app/modules/services/subject/subject.service.ts
+++ src/app/modules/services/subject/subject.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { ISubject } from 'src/app/core';
+
+const SUBJECTS_KEY = 'subjects';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SubjectService {
+  private subjects: ISubject[] = [];
 
-  constructor() { }
+  constructor() {
+    this.loadSubjectsFromStorage();
+  }
+
+  private loadSubjectsFromStorage(): void {
+    const storedSubjects = localStorage.getItem(SUBJECTS_KEY);
+    if (storedSubjects) {
+      this.subjects = JSON.parse(storedSubjects);
+    }
+  }
+
+  getSubjects(): ISubject[] {
+    return this.subjects;
+  }
+
+  addSubject(subject: ISubject): void {
+    subject.id = this.generateId();
+    this.subjects.push(subject);
+    this.saveSubjectsToStorage();
+  }
+
+  updateSubject(updatedSubject: ISubject): void {
+    const index = this.subjects.findIndex((s) => s.id === updatedSubject.id);
+    if (index !== -1) {
+      this.subjects[index] = updatedSubject;
+      this.saveSubjectsToStorage();
+    }
+  }
+
+  deleteSubject(subjectId: number): void {
+    this.subjects = this.subjects.filter((s) => s.id !== subjectId);
+    this.saveSubjectsToStorage();
+  }
+
+  private generateId(): number {
+    return this.subjects.length > 0 ? Math.max(...this.subjects.map((s) => s.id)) + 1 : 1;
+  }
+
+  private saveSubjectsToStorage(): void {
+    localStorage.setItem(SUBJECTS_KEY, JSON.stringify(this.subjects));
+  }
 }
